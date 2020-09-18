ae085f6af6239c64a38dc88e0c3c08ead486eb3e
LizanoLeo123 <leoln123@live.com>
2020-09-18 12:13:23 -0600

    Initial commit

---
+++ Transform1.py
@@ -0,0 +1,20 @@
+import cv2
+
+img = cv2.imread("Resources/FotosMangoReducidas/IMG_0359.jpg", cv2.IMREAD_UNCHANGED)
+
+# Ask for x and y pixels
+xAxis = 0
+yAxis = 0
+try:
+    print("Enter the x axis value: ")
+    xAxis = int(input())
+    print("Enter the y axis value: ")
+    yAxis = int(input())
+except:
+    print("Please enter a valid number")
+
+# Show RGB vector for those pixels
+b,g,r = (img[xAxis, yAxis])
+print ("Red:" + str(r))
+print ("Green:" + str(g))
+print ("Blue:" + str(b))
